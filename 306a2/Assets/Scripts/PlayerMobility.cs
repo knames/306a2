5a289658302702147926e1e7b6520b0f06efa15c
knames <okenso@gmail.com>
2015-10-08 14:43:11 -0600

    Created game
    Have working Skeleton and Spider movement. Need to attach a rocket
    launcher and provide the animations for that, as well as the excessive
    spider explosions.

---
+++ 306a2/Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMobility : MonoBehaviour {
+
+    public float speed;
+    //private Rigidbody2D rb2d;
+
+
+    void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        //rb2d.angularVelocity = 0;
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+        float input = Input.GetAxis("Vertical");
+       GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed * input);
+    }
+	
+}
+
+
